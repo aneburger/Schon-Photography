11cea95969ad94a0241179e8a5cde83c8c9cfcc3
aneburger <aneburger78@gmail.com>
2025-11-28 17:14:23 +0200

    Added files

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+    return (
+        // <BrowserRouter>
+        // 	<div>
+        //     	<Routes>
+        //         		<Route path="/" element={<Home/>}/>
+        //         		<Route path="/plants" element={<Plants/>}/>
+        //     	</Routes>
+        // 	</div>
+        // </BrowserRouter>
+        <div>
+            Hellooooo
+        </div>
+    );
+}
+
+	export default App;
